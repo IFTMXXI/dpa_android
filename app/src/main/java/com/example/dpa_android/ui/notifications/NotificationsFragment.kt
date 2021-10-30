1f5581afc643f72e63066bade760647ab004753e
luiz <luiz.lfcvfernando@gmail.com>
2021-10-30 09:58:50 -0300

    Adcionando Tela de News

--- app/src/main/java/com/example/dpa_android/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/dpa_android/ui/notifications/NotificationsFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.dpa_android.R
 import com.example.dpa_android.databinding.FragmentNotificationsBinding
 
@@ -31,10 +31,10 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val btn: Button = binding.button
+        btn.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.createNews)
+        }
 
 
         return root
